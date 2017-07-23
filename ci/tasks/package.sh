53ef07d9130a3a1a54db768a05b3b05b7de4f999
Ramadass <566484@AMB00677.local>
2017-07-23 16:19:42 +0530

    updated package.sh

--- ci/tasks/package.sh
+++ ci/tasks/package.sh
@@ -4,7 +4,7 @@ set -e +x
 
 pushd movie-fun
   echo "Packaging JAR"
-  ./mvnw clean package -DskipTests
+  mvn clean install -Dmaven.test.skip=true
 popd
 
 jar_count=`find movie-fun/target -type f -name *.war | wc -l`
